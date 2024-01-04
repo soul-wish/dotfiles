f32ce8ddffdd0b19324013ff086cfaa834a360d6
Sergii Lysenko <soulwish.ls@gmail.com>
2024-01-04 09:42:50 -0500

    add wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,61 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = "Tokyo Night Moon"
+-- Tabbar settings
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.hide_tab_bar_if_only_one_tab = true
+config.show_tab_index_in_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+local function tab_title(tab_info)
+	local title = tab_info.tab_title
+	-- if the tab title is explicitly set, take that
+	if title and #title > 0 then
+		return title
+	end
+	-- Otherwise, use the title from the active pane
+	-- in that tab
+	return tab_info.active_pane.title
+end
+wezterm.on("format-tab-title", function(tab, _, _, _, _, max_width)
+	local title = tab_title(tab)
+
+	-- ensure that the titles fit in the available space,
+	-- and that we have room for the edges.
+	title = wezterm.truncate_right(title, max_width - 4)
+
+	return {
+		{ Text = " [" },
+		{ Text = title },
+		{ Text = "] " },
+	}
+end)
+-- Titlebar settings
+config.window_decorations = "RESIZE"
+config.integrated_title_button_style = "MacOsNative"
+-- Window Padding
+config.window_padding = {
+	top = 20,
+	right = 0,
+	bottom = 0,
+	left = 0,
+}
+--Fonts
+config.font = wezterm.font("CaskaydiaCove NF")
+config.font_size = 16
+
+-- and finally, return the configuration to wezterm
+return config
