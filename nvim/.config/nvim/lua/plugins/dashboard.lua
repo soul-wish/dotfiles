43675c8076af9adabf5509f0efa96ea33bbb5d9e
Sergii Lysenko <soulwish.ls@gmail.com>
2024-07-11 23:24:11 -0400

    add ability to go to Lazy straight from the dashboard

--- nvim/.config/nvim/lua/plugins/dashboard.lua
+++ nvim/.config/nvim/lua/plugins/dashboard.lua
@@ -22,11 +22,12 @@ return {
 				header = vim.split(logo, "\n"),
       -- stylua: ignore
       center = {
-        { action = "Telescope find_files",                                    desc = " Find File",       icon = " ", key = "f" },
+        { action = "Telescope find_files",                                     desc = " Find File",       icon = " ", key = "f" },
         { action = "ene | startinsert",                                        desc = " New File",        icon = " ", key = "n" },
         { action = "Telescope oldfiles",                                       desc = " Recent Files",    icon = " ", key = "r" },
         { action = "Telescope live_grep",                                      desc = " Find Text",       icon = " ", key = "g" },
         { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
+        { action = 'Lazy',                                                     desc = " Lazy Updates",    icon = "󰒲 ", key = "l" },
         { action = "qa",                                                       desc = " Quit",            icon = " ", key = "q" },
       },
 				footer = function()
