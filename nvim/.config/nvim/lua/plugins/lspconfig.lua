d3e032d25d21ca3c262f5fba273222cfcbe2fc76
Sergii Lysenko <soulwish.ls@gmail.com>
2024-09-11 10:05:19 -0400

    fix typescript lsp config

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -165,7 +165,7 @@ return { -- LSP Configuration & Plugins
 			--    https://github.com/pmizio/typescript-tools.nvim
 			--
 			-- But for many setups, the LSP (`tsserver`) will work just fine
-			tsserver = {},
+			ts_ls = {},
 			--
 
 			lua_ls = {
