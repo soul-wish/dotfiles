c1bfa672741e0ce32c9e2cb8caff204c5bb6db36
Sergii Lysenko <soulwish.ls@gmail.com>
2024-10-19 22:32:08 -0400

    enable rosepine theme

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,18 +1,31 @@
 return {
 	{
-		"catppuccin/nvim",
-		name = "catppuccin",
+		"rose-pine/neovim",
+		name = "rose-pine",
 		priority = 1000,
 		setup = function()
-			require("catppuccin").setup({
-				flavour = "mocha",
-				integrations = {
-					notify = true,
-				},
+			require("rose-pine").setup({
+				variant = "moon",
 			})
 		end,
 		init = function()
-			vim.cmd.colorscheme("catppuccin-mocha")
+			vim.cmd("colorscheme rose-pine-moon")
 		end,
 	},
+	-- {
+	-- 	"catppuccin/nvim",
+	-- 	name = "catppuccin",
+	-- 	priority = 1000,
+	-- 	setup = function()
+	-- 		require("catppuccin").setup({
+	-- 			flavour = "mocha",
+	-- 			integrations = {
+	-- 				notify = true,
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- 	init = function()
+	-- 		vim.cmd.colorscheme("catppuccin-mocha")
+	-- 	end,
+	-- },
 }
