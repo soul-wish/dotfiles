89dc033362da3b4ad0059cfb8f857a866b33d825
Sergii Lysenko <soulwish.ls@gmail.com>
2025-04-16 06:44:15 -0400

    trying blink for autocompletion

---
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -0,0 +1,42 @@
+return {
+	{
+		"saghen/blink.cmp",
+		-- optional: provides snippets for the snippet source
+		dependencies = "rafamadriz/friendly-snippets",
+
+		-- use a release tag to download pre-built binaries
+		version = "*",
+		-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+		-- build = 'cargo build --release',
+
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			-- 'default' for mappings similar to built-in completion
+			-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+			-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+			-- see the "default configuration" section below for full documentation on how to define
+			-- your own keymap.
+			keymap = { preset = "default" },
+
+			appearance = {
+				-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+				-- Adjusts spacing to ensure icons are aligned
+				nerd_font_variant = "mono",
+			},
+
+			-- default list of enabled providers defined so that you can extend it
+			-- elsewhere in your config, without redefining it, via `opts_extend`
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+				-- optionally disable cmdline completions
+				-- cmdline = {},
+			},
+
+			fuzzy = { implementation = "prefer_rust_with_warning" },
+		},
+		-- allows extending the providers array elsewhere in your config
+		-- without having to redefine it
+		opts_extend = { "sources.default" },
+	},
+}
