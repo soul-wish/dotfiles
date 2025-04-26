2ece02a62a9c57917082a651117278e6a7f882c5
Sergii Lysenko <soulwish.ls@gmail.com>
2025-04-26 14:00:21 -0400

    update conform config

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,11 +1,12 @@
 return { -- Autoformat
 	"stevearc/conform.nvim",
-	lazy = false,
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
 	keys = {
 		{
 			"<leader>f",
 			function()
-				require("conform").format({ async = true, lsp_fallback = true })
+				require("conform").format({ async = true, lsp_format = "fallback" })
 			end,
 			mode = "",
 			desc = "[F]ormat buffer",
@@ -18,10 +19,14 @@ return { -- Autoformat
 			-- have a well standardized coding style. You can add additional
 			-- languages here or re-enable it for the disabled ones.
 			local disable_filetypes = { c = true, cpp = true }
-			return {
-				timeout_ms = 1000,
-				lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-			}
+			if disable_filetypes[vim.bo[bufnr].filetype] then
+				return nil
+			else
+				return {
+					timeout_ms = 500,
+					lsp_format = "fallback",
+				}
+			end
 		end,
 		formatters_by_ft = {
 			lua = { "stylua" },
