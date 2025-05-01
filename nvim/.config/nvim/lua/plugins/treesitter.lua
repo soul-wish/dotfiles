5be1ede7dd8bf7f732549541eaa1104cbca8ea57
Sergii Lysenko <soulwish.ls@gmail.com>
2025-05-01 11:30:20 -0400

    replace telescope with fzf-lua

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return { -- Highlight, edit, and navigate code
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
+	main = "nvim-treesitter.configs", -- Sets main module to use for opts
 	opts = {
 		ensure_installed = {
 			"html",
