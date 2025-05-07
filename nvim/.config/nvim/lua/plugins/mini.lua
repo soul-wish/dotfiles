d0ca1d02ffd349678d4082cfc58b3ddf687e7db6
Sergii Lysenko <soulwish.ls@gmail.com>
2025-05-07 10:33:07 -0400

    remove some plugins, replace with alternatives from snacks

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -7,35 +7,6 @@ return { -- Collection of various small independent plugins/modules
 		--  - va)  - [V]isually select [A]round [)]paren
 		--  - yinq - [Y]ank [I]nside [N]ext [']quote
 		--  - ci'  - [C]hange [I]nside [']quote
-		require("mini.ai").setup({ n_lines = 500 })
-
-		-- Add/delete/replace surroundings (brackets, quotes, etc.)
-		--
-		-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-		-- - sd'   - [S]urround [D]elete [']quotes
-		-- - sr)'  - [S]urround [R]eplace [)] [']
-		require("mini.surround").setup()
-		require("mini.indentscope").setup({
-			symbol = "│",
-			options = { try_as_border = true },
-		})
-		--  Check out: https://github.com/echasnovski/mini.nvim
-	end,
-	init = function()
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = {
-				"dashboard",
-				"fzf",
-				"help",
-				"mason",
-				"neo-tree",
-				"notify",
-				"Trouble",
-				"trouble",
-			},
-			callback = function()
-				vim.b.miniindentscope_disable = true
-			end,
-		})
+		require("mini.ai").setup({ n_lines = 100 })
 	end,
 }
