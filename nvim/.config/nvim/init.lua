c78029a5deff33449b0adddf232c57e8edc9d703
Sergii <sergii.lysenko@dotmatics.com>
2025-09-02 11:01:59 -0400

    disable relative line numbers

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -12,7 +12,7 @@ vim.opt.termguicolors = true
 -- [[ Setting options ]]
 -- Make line numbers default
 vim.opt.number = true
-vim.opt.relativenumber = true
+-- vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = "a"
