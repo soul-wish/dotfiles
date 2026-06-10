a4a4f5c03e3d3be5838e67334e88c60ba3204da3
Sergii Lysenko <soulwish.ls@gmail.com>
2026-06-09 23:04:22 -0400

    add fff.nvim plugin, replace file search and live fuzzy grep

---
+++ nvim/.config/nvim/lua/plugins/fff.lua
@@ -0,0 +1,45 @@
+return {
+	"dmtrKovalenko/fff.nvim",
+	build = function()
+		-- downloads a prebuilt binary or falls back to cargo build
+		require("fff.download").download_or_build_binary()
+	end,
+	opts = {
+		prompt = "> ",
+		title = "Files",
+		layout = {
+			prompt_position = "top",
+		},
+	},
+	lazy = false, -- the plugin lazy-initialises itself
+	keys = {
+		{
+			"<leader>sf",
+			function()
+				require("fff").find_files()
+			end,
+			desc = "Find files",
+		},
+		-- {
+		-- 	"fg",
+		-- 	function()
+		-- 		require("fff").live_grep()
+		-- 	end,
+		-- 	desc = "LiFFFe grep",
+		-- },
+		{
+			"<leader>sg",
+			function()
+				require("fff").live_grep({ grep = { modes = { "fuzzy", "plain" } } })
+			end,
+			desc = "Live fuzzy grep",
+		},
+		-- {
+		-- 	"fc",
+		-- 	function()
+		-- 		require("fff").live_grep({ query = vim.fn.expand("<cword>") })
+		-- 	end,
+		-- 	desc = "Search current word",
+		-- },
+	},
+}
